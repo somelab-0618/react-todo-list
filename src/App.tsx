8339d525bdc1a9f9dcdd2bcf2b5967bdac3e932d
RyosukeSomeya <rx.0.0618@gmail.com>
2022-01-08 11:58:55 +0900

    TodoListのベースを作成

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div className="task-state">
+        <input type="radio" />すべて
+        <input type="radio" />作業中
+        <input type="radio" />完了
+      </div>
+      <table className="task-list">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>コメント</th>
+            <th>状態</th>
+          </tr>
+        </thead>
+      </table>
+      <h2>新規タスクの追加</h2>
+      <input type="text" />
+      <button>追加</button>
     </div>
   );
 }
