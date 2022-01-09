46fefa849ebfde7dad4848040ab6e3442085fd18
RyosukeSomeya <rx.0.0618@gmail.com>
2022-01-10 02:12:52 +0900

    Todo追加機能実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { InputTodo } from './components/InputTodo';
+import { TodoListTable } from './components/TodoListTable';
+import { Todo } from './common/interfaces';
 
-function App() {
+type todoText = string;
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [todoText, setTodoText] = useState<todoText>('');
+
+  const addTodo: () => void = () => {
+    if (todoText === '') return;
+    const newTodo: Todo = {
+      id: todos.length + 1,
+      content: todoText,
+      isDone: false,
+    };
+
+    const newTodoList: Todo[] = [...todos, newTodo];
+    setTodos(newTodoList);
+    setTodoText('');
+  };
+
+  const changeInputText: (e: React.ChangeEvent<HTMLInputElement>) => void = (e) => {
+    setTodoText(e.target.value);
+  };
+
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Todo List</h1>
-      <div className="task-state">
-        <input type="radio" />すべて
-        <input type="radio" />作業中
-        <input type="radio" />完了
+      <div className='task-state'>
+        <input type='radio' />
+        すべて
+        <input type='radio' />
+        作業中
+        <input type='radio' />
+        完了
       </div>
-      <table className="task-list">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>コメント</th>
-            <th>状態</th>
-          </tr>
-        </thead>
-      </table>
+      <TodoListTable todos={todos} />
       <h2>新規タスクの追加</h2>
-      <input type="text" />
-      <button>追加</button>
+      <InputTodo
+        inputText={todoText}
+        changeInputText={changeInputText}
+        addTodo={addTodo}
+      />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/TodoListTable.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Todo } from '../common/interfaces';
+
+interface Props {
+  todos: Todo[];
+}
+
+export const TodoListTable = (props: Props) => {
+  const { todos } = props;
+  return (
+    <>
+      <table className='task-list'>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>コメント</th>
+            <th>状態</th>
+          </tr>
+        </thead>
+        <tbody>
+          {todos.map((todo: Todo, index: number) => {
+            return (
+              <tr key={todo.id}>
+                <td>{todo.id}</td>
+                <td>{todo.content}</td>
+                <td>
+                  {todo.isDone ? <button>完了</button> : <button>作業中</button>}
+                </td>
+                <td>
+                  <button>削除</button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
